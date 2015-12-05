2bd243f4e475dfdd61f2349e4acbebae2a623e23
Tristan Maat <tm@tlater.net>
2015-12-05 17:19:59 +0000

    Add navbar to login page

--- html/index.php
+++ html/index.php
@@ -5,32 +5,12 @@
        include('header.html');
     ?>
     <title>Anything</title>
-  </head>
+  </head>h
 
   <body>
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed"
-                  data-toggle="collapse" data-target="#navbar"
-                  aria-expanded="false" aria-controls="navbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-
-          <a class="navbar-brand" href="/">Home</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li>
-              <a href="/test">Test!</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
+    <?php
+       include('navbar.html');
+    ?>
     <div class="container main-container">
 
       <div class="row">
