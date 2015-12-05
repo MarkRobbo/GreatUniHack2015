8cf670d24db3713cceaf5e11175da69470283659
Mark Robinson <mark@goldtek.co.uk>
2015-12-05 18:35:30 +0000

    Remove code, now works and stores successfully

--- html/login/validateLogin.php
+++ html/login/validateLogin.php
@@ -19,18 +19,15 @@
 			include_once "SteamAPI.class.php";
 			$steamAPI = new SteamAPI();
 			$playerSummary = $steamAPI->getPlayerInfo($loginAttempt);
-			print_r($playerSummary);
 
-			//$_SESSION['name'] = $playerSummary[];
-			//$_SESSION['avatar'] = $playerSummary[];
+			// Store avatar and name
+			$_SESSION['name'] = $playerSummary['personaname'];
+			$_SESSION['avatar'] = $playerSummary['avatarfull'];
 		}
 		else
 		{
 			// Login wasn't attempted and page cannot be accessed without privilages
 			header( 'Location: /login.php?return_url=' . urlencode($_SERVER['REQUEST_URI']) );
 		}
-	} else {
-		echo $_SESSION['steamID'];
-		session_destroy();
 	}
 ?>
