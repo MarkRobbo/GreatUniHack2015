f3e5f1505849d7c5d81051b2a31be6cb2797c990
Tristan Maat <tm@tlater.net>
2015-12-05 20:04:35 +0000

    Make the temporary typeahead content fixed for testing

--- html/sponsor.php
+++ html/sponsor.php
@@ -40,18 +40,19 @@
         </div>
       </div>
 
-      <div class="row">
+      <div id="" class="row hidden">
         <div class="col-md-12">
           <div class="form-group">
             <label for="game">Game</label>
             <input type="text" class="form-control"
-                   id="game" placeholder="Something for now" disabled></input>
+                   id="game" placeholder="" disabled></input>
           </div>
 
           <div class="form-group">
             <label for="charity">Charity</label>
             <input type="text" data-provide="typeahead"
-                   autocomplete="off" id="charity" class="form-control"></input>
+                   autocomplete="off" id="charity"
+                   class="form-control" placeholder="Charity"></input>
           </div>
         </div>
       </div>
@@ -62,5 +63,15 @@
     <script src="/js/bootstrap.min.js"></script>
     <script src="/js/bootstrap3-typeahead.min.js"></script>
     <script src="/js/typeaheadData.js"></script>
+
+    <script type="text/javascript">
+        window.onload(function () {
+            var element = $('#charity');
+            var source = ["test", "this", "code", "something", "more"];
+
+            connectTypeahead(element, source);
+      });
+    </script>
+
   </body>
 </html>
