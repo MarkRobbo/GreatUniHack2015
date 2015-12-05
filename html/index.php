fff3fe4045b7fcc6f519a8d4395779d9cc407acf
Tristan Maat <tm@tlater.net>
2015-12-05 16:51:52 +0000

    Fix home

--- html/index.php
+++ html/index.php
@@ -32,15 +32,22 @@
     </nav>
 
     <div class="container floating-container">
+
       <div class="row">
         <div class="col-md-12">
           <h3>I am a</h3>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6">
           <div class="btn-group" role="group">
             <button type="button" class="btn btn-default">Streamer</button>
             <button type="button" class="btn btn-default">Viewer</button>
           </div>
         </div>
       </div>
+
     </div>
   </body>
 
