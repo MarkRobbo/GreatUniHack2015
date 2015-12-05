edbebb3adad7d609af1dfae217de10fcdacb3ecf
Lauren <lauren.weston@student.manchester.ac.uk>
2015-12-05 19:46:35 +0000

    Got rid of drop down

--- html/player.php
+++ html/player.php
@@ -13,7 +13,7 @@
     ?>
     <div class="container">
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-12">
           <h1 class="row page-header">Username</h1>
         </div>
       </div>
@@ -21,15 +21,12 @@
       <div class="row">
         <div class="col-md-12">
           <div class="btn-group-vertical" role="group">
-            
-						<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							Game <span class="caret"></span>
-						</button>
-						<ul class="dropdown-menu">
-							<li><a href="#">A charity name</a></li>
-							<li><a href="#">Another charity</a></li>
-							<li role="separator" class="divider"></li>
-						</ul>
+						
+						<div class="form-group">
+							<label for="charity">Game</label>
+							<input type="text" data-provide="typeahead"
+										 autocomplete="off" id="charity" class="form-control"></input>
+						</div>
 						
 						<div class="form-group">
 							<label for="charity">Charity</label>
