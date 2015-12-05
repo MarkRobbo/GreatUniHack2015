811b1e6c5c1c20c28ef8d31311db6fe915b4acb4
Mark Robinson <mark@goldtek.co.uk>
2015-12-05 19:33:24 +0000

    Dump session vars

--- html/login/validateLogin.php
+++ html/login/validateLogin.php
@@ -4,6 +4,10 @@
 	include_once "SteamSignIn.class.php";
 	$steamSignIn = new SteamSignIn();
 
+	echo '<pre>';
+	var_dump($_SESSION);
+	echo '</pre>';
+
 	// If the account is not activated yet, ask for their email
 	if (isset($_SESSION['steamID']) && $_SESSION['activated'] == false)
 	{
