a02d4461c4820a545376c795ab943eabe40cec89
Tristan Maat <tm@tlater.net>
2015-12-05 19:30:26 +0000

    I cannot type

--- html/sponsor.php
+++ html/sponsor.php
@@ -60,7 +60,7 @@
     </div>
 
     <script src="/js/jquery.min.js"></script>
-    <script src="/js/bootstrap.min.js"></scirpt>
+    <script src="/js/bootstrap.min.js"></script>
     <script src="/js/bootstrap3-typeahead.min.js"></script>
   </body>
 </html>
