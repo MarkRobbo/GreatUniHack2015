f68b80b418cb455d897577205a9679da5f33a0ed
Mark Robinson <mark@goldtek.co.uk>
2015-12-05 17:40:43 +0000

    Change indent

--- html/login/SteamAPI.class.php
+++ html/login/SteamAPI.class.php
@@ -1,16 +1,16 @@
 <?php
 
-class SteamAPI
-{
-	const BASE_URL = "http://api.steampowered.com/";
-	const API_KEY = "EC8C180193E2429F3107446BEFB941DF";
-
-	// Get the player info of the steam ID
-	public static function getPlayerInfo($steamID)
+	class SteamAPI
 	{
-		$apiResponse = file_get_contents(BASE_URL . "ISteamUser/GetPlayerSummaries/v0002/?key=" . API_KEY . "&steamids=" . $steamID ."&format=json");
-		return json_decode($apiResponse, true);
+		const BASE_URL = "http://api.steampowered.com/";
+		const API_KEY = "EC8C180193E2429F3107446BEFB941DF";
+
+		// Get the player info of the steam ID
+		public static function getPlayerInfo($steamID)
+		{
+			$apiResponse = file_get_contents(BASE_URL . "ISteamUser/GetPlayerSummaries/v0002/?key=" . API_KEY . "&steamids=" . $steamID ."&format=json");
+			return json_decode($apiResponse, true);
+		}
 	}
-}
 
 ?>
