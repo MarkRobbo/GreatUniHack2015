0fa8d47ba49461bfc3a0128fe9eea199d25c4f28
Tristan Maat <tm@tlater.net>
2015-12-06 01:16:52 +0000

    Debugging

--- html/login/getCharities.php
+++ html/login/getCharities.php
@@ -8,6 +8,5 @@ $jgAPI = new JustGivingAPI();
 //$charityNames = $jgAPI->searchCharities($searchString, 10);
 $charityNames = $jgAPI->searchCharities('a', 10);
 
-// var_dump($charityNames);
-echo $searchString
+var_dump($charityNames);
 ?>
