b3fd96346be0fc0d010118edbb694d348881c5ee
Tristan Maat <tm@tlater.net>
2015-12-06 02:53:36 +0000

    Try more samples

--- html/login/getCharities.php
+++ html/login/getCharities.php
@@ -5,7 +5,7 @@ $searchString = $_GET["typed"];
 
 $jgAPI = new JustGivingAPI();
 
-$charityNames = $jgAPI->searchCharities($searchString, 10);
+$charityNames = $jgAPI->searchCharities($searchString, 50);
 
 echo json_encode($charityNames);
 ?>
