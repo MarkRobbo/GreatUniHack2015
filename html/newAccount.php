ba370f2fc0c05a33eb5abe8c095a90fa1fb9f379
Tristan Maat <tm@tlater.net>
2015-12-06 05:58:46 +0000

    Make container more generic

--- html/newAccount.php
+++ html/newAccount.php
@@ -17,27 +17,29 @@
     ?>
 
     <div class="container">
-      <div class="col-md-12">
-        <h2>Account Activation</h2>
-        <p>Thank you for signing up for achieve4charity!</p>
-        <p>To begin making or accepting challenges, please provide your email address:</p>
-        <form id="newAccount" role="form" action="newAccount.php" method="POST">
-          <div class="form-group">
-            <label for="email">E-Mail</label>
-            <input type="email" name="email" class="form-control"
-                   placeholder="E-Mail" id="email"></input>
-          </div>
+      <h1 class="row page-header">Account Activation</h1>
+      <div class="row">
+        <div class="col-md-12">
+          <p>Thank you for signing up for achieve4charity!</p>
+          <p>To begin making or accepting challenges, please provide your email address:</p>
+          <form id="newAccount" role="form" action="newAccount.php" method="POST">
+            <div class="form-group">
+              <label for="email">E-Mail</label>
+              <input type="email" name="email" class="form-control"
+                     placeholder="E-Mail" id="email"></input>
+            </div>
 
-          <div class="form-group">
-            <label for="charity">Charity</label>
-            <input type="text" id="charity" data-provide="typeahead"
-                   autocomplete="off" class="form-control" id="charity"
-                   placeholder="Charity goal"></input>
-          </div>
-          <input class="hidden" id="charity_id" name="charity_ID">
-          <button form="newAccount" type="submit" value="Submit"
-                  class="btn btn-primary">
-        </form>
+            <div class="form-group">
+              <label for="charity">Charity</label>
+              <input type="text" id="charity" data-provide="typeahead"
+                     autocomplete="off" class="form-control" id="charity"
+                     placeholder="Charity goal"></input>
+            </div>
+            <input class="hidden" id="charity_id" name="charity_ID">
+            <button form="newAccount" type="submit" value="Submit"
+                    class="btn btn-primary">Submit</button>
+          </form>
+        </div>
       </div>
     </div>
 
