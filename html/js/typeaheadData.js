22bc4432049c6a15268320b2b7a8d4a83318226e
Tristan Maat <tm@tlater.net>
2015-12-06 01:48:19 +0000

    Debugging

--- html/js/typeaheadData.js
+++ html/js/typeaheadData.js
@@ -2,6 +2,8 @@ function connectTypeahead (element, source) {
     $.get(source, function(data) {
         data = JSON.parse(data);
 
+        console.log(data);
+
         element.typeahead({
             source: data
         });
