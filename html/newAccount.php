804806f7df1584907bf74b6168f608d259b143d6
Tristan Maat <tm@tlater.net>
2015-12-06 05:56:50 +0000

    Make container more generic

--- html/newAccount.php
+++ html/newAccount.php
@@ -16,7 +16,7 @@
        include('navbar.html');
     ?>
 
-    <div class="container main-container">
+    <div class="container">
       <div class="col-md-12">
         <h2>Account Activation</h2>
         <p>Thank you for signing up for achieve4charity!</p>
