8c69fd8d9a9e5d377fa951e968802aeb3c86e7f4
Mark Robinson <mark@goldtek.co.uk>
2015-12-06 02:42:03 +0000

    show just errors

--- html/login/validateLogin.php
+++ html/login/validateLogin.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 
-	error_reporting(-1); // reports all errors
+	error_reporting(E_ERROR); // reports all errors
 ini_set("display_errors", "1"); // shows all errors
 ini_set("log_errors", 1);
 ini_set("error_log", "/tmp/php-error.log");
