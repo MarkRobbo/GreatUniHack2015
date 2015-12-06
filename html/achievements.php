cf12360ac39f7dbf2e37566f910c71819e919d52
Tristan Maat <tm@tlater.net>
2015-12-06 05:31:00 +0000

    Tons of scripts

--- html/achievements.php
+++ html/achievements.php
@@ -51,6 +51,10 @@
 
     </div>
 
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/bootstrap3-typeahead.min.js"></script>
+    <script src="/js/typeaheadData.js"></script>
     <script type="text/javacsript" src="/js/bootstrap-table.min.js"></script>
   </body>
 </html>
